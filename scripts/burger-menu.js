0fe18e16658f30bc11b2f759f2269a1b2f8ec0eb
dreamer_dk <dreamer_dk@mail.ru>
2025-06-24 12:30:41 +0300

    edit burger menu

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -1,8 +1,8 @@
 const btn = document.querySelector(".burger-menu")
-const links = document.querySelector(".header-burger-menu-links")
+const links = document.querySelector(".header-burger-menu")
 btn.addEventListener("click", () => {
     if (links.style.top == "-3000px") {
-        links.style.top = "95px"
+        links.style.top = "98px"
     } else {
         links.style.top = "-3000px"
     }
