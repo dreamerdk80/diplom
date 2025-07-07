d977aec6486fb1054622778181c7526f6ca3cfc5
dreamerdk80 <dreamer_dk@mail.ru>
2025-07-07 12:32:05 +0300

    single page

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,20 +1,50 @@
+const langDescription = document.querySelector(".lang-description")
 const cards = document.querySelector(".cards")
+const php = document.querySelector("#php")
+const javascript = document.querySelector("#javascript")
+const langBtn = document.querySelectorAll(".lang-btn")
+console.log(langBtn[0])
 
 window.addEventListener("load", async () => {
     const result = await fetch("json/cards.json")
         .then(res => res.json())
 
-    result.cards.forEach(card => {
-        cards.innerHTML += `
-            <figure class="card">
-                <img src="${card.image}" alt="">
+    for (let i = 0; i < langBtn.length; i++) {
+        langBtn[i].addEventListener("click", () => {
+            const language = langBtn[i].id
+            const card = result[language]
+            cards.innerHTML = ""
 
-                <figcaption>
-                    <p class="card-author">${card.author}</p>
+            langDescription.innerHTML = `
+                <div class="description">
+                    <figure class="title">
+                        <img src="${card[0].icon}" alt="">
 
-                    <h2 class="card-name">${card.title}</h2>
-                </figcaption>
-            </figure>
-        `
-    })
+                        <figcaption>
+                            <h1>${card[0].title}</h1>
+                        </figcaption>
+                    </figure>
+
+                    <p>${card[0].text}</p>
+                </div>
+            `
+
+            for (let j = 1; j < card.length; j++) {
+                cards.innerHTML += `
+                    <figure class="card">
+                        <img src="${card[j].image}" alt="">
+
+                        <figcaption>
+                            <p class="card-author">${card[j].author}</p>
+
+                            <h2 class="card-name">${card[j].title}</h2>
+                        </figcaption>
+                    </figure>
+                `
+            }
+        })
+    }
+    
+    
+    
 })
