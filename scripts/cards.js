e40d3c3c6d8f1cc67a9efa18df68d1a02bddab46
dreamerdk80 <dreamer_dk@mail.ru>
2025-07-08 01:18:57 +0300

    add function

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,50 +1,59 @@
 const langDescription = document.querySelector(".lang-description")
 const cards = document.querySelector(".cards")
-const php = document.querySelector("#php")
-const javascript = document.querySelector("#javascript")
 const langBtn = document.querySelectorAll(".lang-btn")
-console.log(langBtn[0])
+let result
 
 window.addEventListener("load", async () => {
-    const result = await fetch("json/cards.json")
+    result = await fetch("json/cards.json")
         .then(res => res.json())
 
+    generateCards()
+
+    
+})
+
+function generateDescriptionHTML(card) {
+    return `
+        <div class="description">
+            <figure class="title">
+                <img src="${card.icon}" alt="">
+
+                <figcaption>
+                    <h1>${card.title}</h1>
+                </figcaption>
+            </figure>
+
+            <p>${card.text}</p>
+        </div>
+    `
+}
+
+function generateCardHTML(card) {
+    return `
+        <figure class="card">
+            <img src="${card.image}" alt="">
+
+            <figcaption>
+                <p class="card-author">${card.author}</p>
+
+                <h2 class="card-name">${card.title}</h2>
+            </figcaption>
+        </figure>
+    `
+}
+
+function generateCards() {
     for (let i = 0; i < langBtn.length; i++) {
         langBtn[i].addEventListener("click", () => {
             const language = langBtn[i].id
             const card = result[language]
             cards.innerHTML = ""
 
-            langDescription.innerHTML = `
-                <div class="description">
-                    <figure class="title">
-                        <img src="${card[0].icon}" alt="">
-
-                        <figcaption>
-                            <h1>${card[0].title}</h1>
-                        </figcaption>
-                    </figure>
-
-                    <p>${card[0].text}</p>
-                </div>
-            `
+            langDescription.innerHTML = generateDescriptionHTML(card[0])
 
             for (let j = 1; j < card.length; j++) {
-                cards.innerHTML += `
-                    <figure class="card">
-                        <img src="${card[j].image}" alt="">
-
-                        <figcaption>
-                            <p class="card-author">${card[j].author}</p>
-
-                            <h2 class="card-name">${card[j].title}</h2>
-                        </figcaption>
-                    </figure>
-                `
+                cards.innerHTML += generateCardHTML(card[j])
             }
         })
     }
-    
-    
-    
-})
+}
